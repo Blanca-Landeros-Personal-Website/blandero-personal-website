fefe1632762cee2c108bd3e3e24f6b352020c11c
blandero <landerosb08@gmail.com>
2023-09-21 17:11:17 -0600

    Created part II branch from Bloack 12 lecture

---
+++ javatPrompt.js
@@ -0,0 +1,28 @@
+// Prompt: Print integers one-to-N, but print “Fizz” if an integer is divisible by three, “Buzz” if an integer is divisible by five, and “FizzBuzz” if an integer is divisible by both three and five
+
+
+
+//NOTE: FizzBuzz is a common interview question that has been claimed to weed out a significant percentage of candidates!
+
+//Remember the REACTO method: 
+// Restate, Examples, Approach, Code, Test, Optimize.  
+
+function fizzBuzz (n) {
+    let wordPrint += ';'
+    let numPrint = 1
+
+    //let n 
+    if (n % 3 === 0) {
+
+    }
+    //if n is divisible by 3, contact fizz
+    
+    //if n is divisible by 5, concact Buzz
+    
+    //else print n
+
+}  
+
+console.log(fizzBuzz(15))
+// in term run: npm node reacto.js
+// or npm run reacto.js OR node -v OR 
